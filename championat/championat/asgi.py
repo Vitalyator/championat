7f757f2b12c7f4a8a824e253f736b5ade8c96c61
Ivan Kotov <kotovivan1995@gmail.com>
2021-09-10 00:38:53 +0300

    create project

---
+++ championat/championat/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for championat project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'championat.settings')
+
+application = get_asgi_application()
